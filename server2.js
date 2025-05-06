af4b6b36380735a68ca65e87dc29a2c85ace93b0
simplemoon403 <69460118+simplemoon403@users.noreply.github.com>
2025-05-06 09:14:32 +0200

    server2.js

--- server2.js
+++ server2.js
@@ -13,7 +13,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.post('/log-click', (req, res) => {
     const { imageId, clickTime, clickCount } = req.body;
     const logEntry = `ImageId: ${imageId}, ClickTime: ${clickTime}\n`;
-    fs.appendFile('C:/Users/jakub.zimandl/click-tracking-server/odpovedi.csv', logEntry, (err) => {
+   
+    /* Maxi, tady musíš pak upravit cestu na csv pro zápis odpovědí */ 
+    fs.appendFile('/odpovedi.csv', logEntry, (err) => { 
         if (err) {
             console.error('Chyba při zápisu do souboru:', err);
             res.status(500).json({ success: false, error: 'Chyba při zápisu do souboru' });
